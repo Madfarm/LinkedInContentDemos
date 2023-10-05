88056042ef1c107c171c0939b0e2694bd820a285
Madfarm <anthony.scott1005@gmail.com>
2023-10-05 14:06:11 -0400

    Update the dbcontext to incorporate the new extended user model

--- AuthAPI/Data/AppDbContext.cs
+++ AuthAPI/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +9,7 @@ namespace AuthAPI.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<IdentityUser> Users { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder); 
