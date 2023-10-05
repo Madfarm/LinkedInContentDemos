fbb428603f6ae260aac60a97d23f1e97723a05eb
Madfarm <anthony.scott1005@gmail.com>
2023-10-05 14:33:50 -0400

    Add UserDto

---
+++ AuthAPI/Models/Dtos/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthAPI.Models.Dtos
+{
+    public class UserDto
+    {
+        // IDs in Identity are Guids so it must be a string here
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
