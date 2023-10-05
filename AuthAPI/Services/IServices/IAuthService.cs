00a7cb67d1bbd52bc470320559f741ceb9098f94
Madfarm <anthony.scott1005@gmail.com>
2023-10-05 18:46:09 -0400

    Create IAuthService

---
+++ AuthAPI/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using AuthAPI.Models.Dtos;
+
+namespace AuthAPI.Services.IServices
+{
+    public interface IAuthService
+    {
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        public Task<string> Register(RegistrationRequestDto registrationRequestDto);
+    }
+}
