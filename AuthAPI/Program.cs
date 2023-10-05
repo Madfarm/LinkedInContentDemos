2f928312d0a16908b2ee56facdbcf626bac25ffb
Madfarm <anthony.scott1005@gmail.com>
2023-10-05 14:14:25 -0400

    Add identity and dbcontext to application container

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,7 +1,16 @@
+using AuthAPI.Data;
+using AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
