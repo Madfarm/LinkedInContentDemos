94019ae26725e285b25819d7ce3158eb66d4a85a
Madfarm <anthony.scott1005@gmail.com>
2023-10-06 12:11:22 -0400

    Scaffold auth controller

---
+++ AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using AuthAPI.Models.Dtos;
+using AuthAPI.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly ResponseDto _response;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+            _response = new ResponseDto();
+        }
+
+
+        [HttpPost("register")]
+        public async IActionResult Register([FromBody] RegistrationRequestDto request)
+        {
+            return NoContent(); 
+        }
+    }
+}
