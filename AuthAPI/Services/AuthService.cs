30d1da33a3aea1cd8098886d1f3f4a9f29f5def7
Madfarm <anthony.scott1005@gmail.com>
2023-10-06 11:12:08 -0400

    Inject required dependencies into Auth service

--- AuthAPI/Services/AuthService.cs
+++ AuthAPI/Services/AuthService.cs
@@ -1,10 +1,22 @@
-﻿using AuthAPI.Models.Dtos;
+﻿using AuthAPI.Data;
+using AuthAPI.Models;
+using AuthAPI.Models.Dtos;
 using AuthAPI.Services.IServices;
+using Microsoft.AspNetCore.Identity;
 
 namespace AuthAPI.Services
 {
     public class AuthService : IAuthService
     {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
         public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             throw new NotImplementedException();
