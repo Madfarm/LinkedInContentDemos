2a43485491475f7286b343f28ccb113362799ff7
Madfarm <anthony.scott1005@gmail.com>
2023-10-07 18:50:57 -0400

    Scaffold the jwt token generator service

---
+++ AuthAPI/Services/IServices/JwtTokenGenerator.cs
@@ -0,0 +1,18 @@
+﻿using AuthAPI.Models;
+
+namespace AuthAPI.Services.IServices
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtTokenGenerator(JwtOptions jwtOptions)
+        {
+            _jwtOptions = jwtOptions;
+        }
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+            
+        }
+    }
+}
